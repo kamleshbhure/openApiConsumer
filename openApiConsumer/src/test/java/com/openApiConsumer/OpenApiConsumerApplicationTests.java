961d80fc3f93a4230b13d8a7717504bda87bf02e
Kamlesh Bhure <kamlesh.bhure@gmail.com>
2021-07-26 11:54:32 +0530

    first commit

---
+++ openApiConsumer/src/test/java/com/openApiConsumer/OpenApiConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openApiConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
